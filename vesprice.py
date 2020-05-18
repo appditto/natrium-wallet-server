ad4a0932a735d6e573f3979aed2a2973e17370e4
Brandon Berhent <bbedward@gmail.com>
2020-05-18 15:23:12 -0400

    Move ARS price to singular cronjob

--- vesprice.py
+++ vesprice.py
@@ -7,6 +7,7 @@ rdata = redis.StrictRedis(host=os.getenv(
     'REDIS_HOST', 'localhost'), port=6379, db=int(os.getenv('REDIS_DB', '2')))
 
 dolartoday_price = 'https://s3.amazonaws.com/dolartoday/data.json'
+dolarsi_ars_prices='https://www.dolarsi.com/api/api.php?type=valoresprincipales'
 
 def dolartoday_bolivar():
     response = json.loads(requests.get(url=dolartoday_price).text)
@@ -20,7 +21,19 @@ def dolartoday_bolivar():
     print(rdata.hset("prices", "dolartoday:usd-ves", bolivarprice),
           "DolarToday USD-VES", bolivarprice)
 
+def dolarsi_ars():
+    response = json.loads(requests.get(url=dolarsi_ars_prices).text)
+    print(response)
+    try:
+        price_ars_raw = response[1]['casa']['venta']
+    except KeyError:
+        print("Invalid response " + str(response))
+        return
+    price_ars = price_ars_raw.replace('.','').replace(',','.')
+    print(rdata.hset("prices", "dolarsi:usd-ars", price_ars),"DolarSi USD-ARS", price_ars)
 
 dolartoday_bolivar()
 print("DolarToday USD-VES:", rdata.hget("prices",
                                         "dolartoday:usd-ves").decode('utf-8'))
+dolarsi_ars()
+print("DolarSi USD-ARS:", rdata.hget("prices", "dolarsi:usd-ars").decode('utf-8'))
