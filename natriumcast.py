972f52997c300af761082e72395bc5eec0b5cb1d
bbedward <bbedward@gmail.com>
2021-03-12 20:24:29 -0500

    Change node

--- natriumcast.py
+++ natriumcast.py
@@ -477,6 +477,9 @@ async def http_api(r: web.Request):
         return web.HTTPInternalServerError(reason=f"Something went wrong {str(sys.exc_info())}")
 
 async def callback_ws(app: web.Application, data: dict):
+    log.server_logger.info(f"Received WS message", str(data))
+    return
+    
     if 'block' in data and 'link_as_account' in data['block']:
         link = data['block']['link_as_account']
         if app['subscriptions'].get(link):
