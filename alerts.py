0dc42d45a047bb69319c93bce4edda4651951f1b
yekta <40706601+yekta@users.noreply.github.com>
2021-03-24 22:44:44 +0300

    Update alerts.py

--- alerts.py
+++ alerts.py
@@ -17,6 +17,19 @@ ACTIVE_ALERTS = [
       "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
     }
   }
+  {
+    "id":2,
+    "active": True,
+    "priority": HIGH_PRIORITY,
+                             # yyyy, M,  D,  H,  M,  S, MS
+    "timestamp": int((datetime(2021, 3, 24, 1, 0, 0, 0, tzinfo=timezone.utc) - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds() * 1000),
+    "en": {
+      "title": "Testing",
+      "short_description": "Due to ongoing issues with the Nano network, your transactions may be delayed.",
+      "long_description": "The Nano network is experiencing issues caused by a prolonged, ongoing period of spam transactions.\n\nTransactions may be significantly delayed, up to several days. We will keep our users updated with new information as the Nano team provides it.\n\nYou can read more by tapping \"Read more\" below.\n\nNatrium is not associated with Nano Foundation or its developers, so even though our service is working normally it still depends on the Nano network to behave normally. We appreciate your patience during this time.",
+      "link": "https://appditto.com/blog/natrium-status-and-state-of-the-nano-network",
+    }
+  }
 ]
 
 def get_active_alert(lang: str):
