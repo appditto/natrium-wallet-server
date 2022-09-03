79cf6a9752a79f68685e8389ae945206dde30382
Brandon Berhent <bbedward@gmail.com>
2022-09-02 23:13:43 -0400

    Un-needed

--- controller/ws_c.go
+++ controller/ws_c.go
@@ -324,14 +324,6 @@ func (c *Client) writePump() {
 				return
 			}
 			w.Write(message)
-
-			// Add queued chat messages to the current websocket message.
-			n := len(c.Send)
-			for i := 0; i < n; i++ {
-				w.Write(newline)
-				w.Write(<-c.Send)
-			}
-
 			if err := w.Close(); err != nil {
 				return
 			}
