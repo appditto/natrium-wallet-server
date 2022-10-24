2c457d6854b278cec827e4e9e75c19f3419755bf
BB <bbedward@gmail.com>
2022-10-24 13:33:27 -0500

    Update account_history_request.go

--- models/account_history_request.go
+++ models/account_history_request.go
@@ -2,9 +2,10 @@ package models
 
 // rpc account_history
 type AccountHistory struct {
-	Action  string `json:"action" mapstructure:"action"`
-	Account string `json:"account" mapstructure:"account"`
-	Count   *int   `json:"count,omitempty" mapstructure:"count,omitempty"`
-	Raw     *bool  `json:"raw,omitempty" mapstructure:"raw,omitempty"`
-	Reverse *bool  `json:"reverse,omitempty" mapstructure:"reverse,omitempty"`
+	Action  string 	`json:"action" mapstructure:"action"`
+	Account string 	`json:"account" mapstructure:"account"`
+	Count   *int   	`json:"count,omitempty" mapstructure:"count,omitempty"`
+	Raw     *bool  	`json:"raw,omitempty" mapstructure:"raw,omitempty"`
+	Reverse *bool  	`json:"reverse,omitempty" mapstructure:"reverse,omitempty"`
+	Head 	*string	`json:"head,omitempty" mapstructure:"head,omitempty"`
 }
